43e7ffeecab9ac5aaccdba38cd7a03b93cbb2ce8
Kacper Warda <kacper.warda@logintegra.com>
2023-05-13 10:57:21 +0200

    initial commit

---
+++ src/main/java/com/example/bestcrmintheunivers/BestCrmInTheUniversApplication.java
@@ -0,0 +1,13 @@
+package com.example.bestcrmintheunivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestCrmInTheUniversApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestCrmInTheUniversApplication.class, args);
+    }
+
+}
